8a57465f458c45a4ca46ccb0bee406d859a932a9
Agata Liberska <liberska@gmail.com>
2021-04-01 11:50:05 +0100

    update number of pageviews on slider value change

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-const slider = document.getElementById('pageviews');
+const pricing = {
+    views: [
+        {
+            views: "10k",
+            price: 8,
+        },
+        {
+            views: "50K",
+            price: 12,
+        },
+        {
+            views: "100K",
+            price: 16,
+        },
+        {
+            views: "500k",
+            price: 24,
+        },
+        {
+            views: "1m",
+            price: 36
+        }   
+    ],
+    discount: 0.25,
+}
+
+const slider = document.getElementById('pageviews-slider');
+const pageviews = document.querySelector('.js-views');
+const price = document.querySelector('.js-price');
 
 function fillSlider() {
     const sliderProgress = 'hsl(174, 77%, 80%)';
@@ -12,8 +40,14 @@ function fillSlider() {
         ${sliderBackground} ${breakpoint})`;
 }
 
+function updateViews(index) {
+    pageviews.innerHTML = pricing.views[index].views;
+}
+
 fillSlider();
 
-slider.addEventListener('change', () => {
+slider.addEventListener('change', (e) => {
 	fillSlider();
+    updateViews(e.target.value);
+    //updatePrice(e.target.value);
 });
